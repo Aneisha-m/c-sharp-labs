d50d8f98c4c32482f8b9837bfacc766c0d1069a6
Aneisha <46450865+Aneisha-m@users.noreply.github.com>
2019-02-17 19:32:32 +0000

    completed lab commit

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //strings
+            string str = "Hello World";
+            string str2 = new string('c', 20);
+            //Integer
+            int intDataType = 5;
+            Int32 anotherType = 6; //CLR 
+            uint myValue = 9;
+            //Float
+            float num1 = 1.347f; //32
+            double num2 = 1.34567894300303; //64
+
+            
+        }
+    }
+}
